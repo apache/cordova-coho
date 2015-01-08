fd88d89048a761626b35c6e51d0f0d433fb0e261
Staci Cooper <smcooper@us.ibm.com>
2015-01-08 15:49:37 -0500

    CB-7949 add cordova-local to tools release process

--- src/repoutil.js
+++ src/repoutil.js
@@ -355,6 +355,13 @@ var toolRepos = [
         id: 'coho',
         repoName: 'cordova-coho',
         jiraComponentName: 'Coho'
+    }, {
+        title: 'Cordova Utility',
+        id: 'utility',
+        repoName: 'cordova-utility',
+        ratExcludes: [
+            'fixtures'
+        ]
     }
 ];
 

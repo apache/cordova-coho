aeab259d25fde8554ac83bc3168f8f64fe05d876
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2016-01-11 17:13:35 +0300

    Fixes incorrect check for platform id in `prepare-release-branch` command

--- src/platform-release.js
+++ src/platform-release.js
@@ -129,7 +129,7 @@ function *updateRepoVersion(repo, version) {
             shelljs.sed('-i', /VERSION.*=.*/, 'VERSION="' + version + '"', path.join('bin', 'templates', 'scripts', 'cordova', 'version'));
         } else if (repo.id == 'blackberry') {
             shelljs.sed('-i', /VERSION.*=.*;/, 'VERSION = "' + version + '";', path.join('bin', 'templates', 'project','cordova', 'lib', 'version.js'));
-        } else if (repo.id == 'firefoxos' || repo.id == 'browser' || 'ubuntu') {
+        } else if (repo.id == 'firefoxos' || repo.id == 'browser' || repo.id == 'ubuntu') {
             shelljs.sed('-i', /VERSION.*=.*;/, 'VERSION = "' + version + '";', path.join('bin', 'templates', 'project','cordova', 'version'));
         } else if (repo.id == 'windows') {
             if(fs.existsSync(path.join('template', 'cordova', 'version'))) {
